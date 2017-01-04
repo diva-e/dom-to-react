916c0ecce9840a54dbd0feb84ec97d8f69ceee37
Kristof Dreier <kontakt@kristofdreier.de>
2017-01-04 13:36:16 +0100

    added parser as fourth argument

--- src/dom-to-react/index.js
+++ src/dom-to-react/index.js
@@ -48,16 +48,16 @@ class Dom2React {
     const result = [];
 
     this.tests.forEach((test) => {
-      if (test.condition(node, key)) {
+      if (test.condition(node, key, level, this)) {
         if (typeof test.modify === 'function') {
-          node = test.modify(node, key, level);
+          node = test.modify(node, key, level, this);
           if (!(node instanceof Node)) {
             node = _node;
             console.warn('The `modify`-method always must return a valid DomNode (instanceof Node) - your modification will be ignored (Hint: if you want to render a React-component, use the `action`-method instead)');
           }
         }
         if (typeof test.action === 'function') {
-          result.push(test.action(node, key, level));
+          result.push(test.action(node, key, level, this));
         }
       }
     });
